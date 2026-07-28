HabiRabbu/DroppedNeedle

feat: support serving under a reverse-proxy base path
Add a BASE_PATH setting so DroppedNeedle can be served under a subpath
(e.g. /droppedneedle) behind a reverse proxy instead of only at the domain
root. Empty/unset (the default) serves at the root exactly as before.

The SvelteKit static build bakes paths.base at build time, so the image
builds the frontend with a placeholder token that the entrypoint rewrites
to the runtime BASE_PATH; a single prebuilt image works at any subpath with
no rebuild. All in-app links, goto() calls, API/EventSource URLs and
browser-facing redirects are made base-aware (no-ops when the base is empty).
The backend sets FastAPI root_path so generated URLs (docs, OAuth redirect
URIs) carry the prefix. The proxy is expected to forward the subpath stripped.

frontend/src/lib/components/library/LibraryDashboard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import {
 		Music,
 		Clock,
@@ -113,7 +114,7 @@
 			</div>
 			{#if showAttention}
 				<a
-					href="/library/unmatched"
+					href={resolve('/library/unmatched')}
 					class="ml-auto flex items-center gap-1.5 rounded-full border border-warning/40 bg-warning/10 px-3 py-1 text-xs font-semibold text-warning transition-colors hover:bg-warning/20"
 				>
 					<AlertTriangle class="h-3.5 w-3.5" />

manuelrr1995/DroppedNeedle-Noauth

frontend/src/lib/components/library/LibraryDashboard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import {
 		Music,
 		Clock,
@@ -102,7 +103,7 @@
 			</div>
 			{#if showAttention}
 				<a
-					href="/library/review"
+					href={resolve('/library/review')}
 					class="ml-auto flex items-center gap-1.5 rounded-full border border-warning/40 bg-warning/10 px-3 py-1 text-xs font-semibold text-warning transition-colors hover:bg-warning/20"
 				>
 					<AlertTriangle class="h-3.5 w-3.5" />